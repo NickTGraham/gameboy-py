ce72923734422de010639760f8533b64dfa7282f
Nick Graham <nickdud341@gmail.com>
2016-05-15 22:09:34 -0400

    Implemented ld

--- cpu.py
+++ cpu.py
@@ -3,27 +3,27 @@ class Registers:
     """Class to hold the registers of the CPU, and allow easy access to them individually and as pairs"""
 
     def __init__(self):
-        self.a = 0
-        self.b = 0
-        self.c = 0
-        self.d = 0
-        self.e = 0
-        self.f = 0
-        self.h = 0
-        self.l = 0
+        self.a = 0 # 111
+        self.b = 0 # 000
+        self.c = 0 # 001
+        self.d = 0 # 010
+        self.e = 0 # 011
+        self.f = 0 #
+        self.h = 0 # 100
+        self.l = 0 # 101
         self.pc = 0x0000
         self.sp = 0xFFFE
 
     def getReg(self, reg):
         return {
-                "a": self.a,
-                "b": self.b,
-                "c": self.c,
-                "d": self.d,
-                "e": self.e,
+                0b111: self.a,
+                0b000: self.b,
+                0b001: self.c,
+                0b010: self.d,
+                0b110: self.e,
                 "f": self.f,
-                "h": self.h,
-                "l": self.l,
+                0b100: self.h,
+                0b101: self.l,
                 "pc": self.pc,
                 "sp": self.sp
                 }[reg]
@@ -37,28 +37,28 @@ class Registers:
                 }[pair]
 
     def setReg(self, reg, val):
-        if (reg == "a"):
+        if (reg == 0b111):
             self.a = val
 
-        elif (reg == "b"):
+        elif (reg == 0b000):
             self.b = val
 
-        elif (reg == "c"):
+        elif (reg == 0b001):
             self.c = val
 
-        elif (reg == "d"):
+        elif (reg == 0b010):
             self.d = val
 
-        elif (reg == "e"):
+        elif (reg == 0b011):
             self.e = val
 
         elif (reg == "f"):
             self.f = val
 
-        elif (reg == "h"):
+        elif (reg == 0b100):
             self.h = val
 
-        elif (reg == "l"):
+        elif (reg == 0b101):
             self.l = val
 
         elif (reg == "sp"):
@@ -94,6 +94,11 @@ class CPU():
         self.n = self.inst #get n in the event it is needed.
 
     def execute(self):
+        #there are now switch case statements in python so...
+        if(self.opcode == 1 and self.r != 0b110 and self.rb != 0b110): #load instruction
+            tmp = self.reg.getReg(self.rp)
+            self.reg.setReg(self.r, tmp)
+
         print(self.opcode, self.r, self.rp, self.n)
 
 
@@ -116,9 +121,9 @@ class Memory():
 
 x = Registers()
 cp = CPU()
-print(x.getReg("b"))
-x.setReg("b", 5)
-print(x.getReg("b"))
+print(x.getReg(0b000))
+x.setReg(0b000, 5)
+print(x.getReg(0b000))
 print(x.getPair("bc"))
 inst = 192
 print((inst&192) >> 6)
